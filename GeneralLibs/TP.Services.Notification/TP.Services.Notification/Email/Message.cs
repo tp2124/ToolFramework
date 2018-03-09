1fa7db8ef3c8d7eb46be84bf8d1823b4580ff30d
Travis Primm <tp2124@gmail.com>
2018-03-08 21:46:47 -0800

    Stub Notification Service
    Adding in notification service.
    +Fixing solution to no longer reference temp projects.

---
+++ GeneralLibs/TP.Services.Notification/TP.Services.Notification/Email/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP.Services.Notification.Email
+{
+    /// <summary>
+    /// Mail object to be populated with the contents of the e-mail.
+    /// </summary>
+    public class Message
+    {
+        public string Body;
+        public string Subject;
+        public IEnumerable<string> Recipients;
+        public IEnumerable<string> CCRecipients;
+        public IEnumerable<string> BCCRecipients;
+        public string Sender;
+    }
+}
